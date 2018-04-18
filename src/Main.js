7211a1c007f408be0878d5a1f30e19479544998e
Carlos <aquinoqcd29@gmail.com>
2018-04-17 23:19:31 -0400

    Add router to App main component

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,24 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class Main extends Component {
-  
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          data: []
+        }
+      }
+    
+      componentDidMount() {
+        axios.get('https://techcrunch.com/wp-json/tc/mobile/v1/posts/featured')
+          .then(data => {
+            console.log(data.data);
+            this.setState({data: data.data.posts});
+          })
+          .catch(_ => console.log('error...'));
+      }
+
     render() {
       return (
         <div>
