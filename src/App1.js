8cdeb7dea3ba2985f6b5d9938f50dbe0eae34c58
Carlos <aquinoqcd29@gmail.com>
2018-04-17 23:08:55 -0400

    Creation of two base components

---
+++ src/App1.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class App1 extends Component {
+  
+    render() {
+      return (
+        <div>
+            Detail article
+        </div>
+      );
+    }
+  }
+  
+  export default App1;
---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Main extends Component {
+  
+    render() {
+      return (
+        <div>
+            Main section
+        </div>
+      );
+    }
+  }
+  
+  export default Main;
