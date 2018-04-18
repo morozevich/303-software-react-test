15714bc2b0dde2dc12e709ceb73fde72387cbc28
Carlos <aquinoqcd29@gmail.com>
2018-04-17 23:27:12 -0400

    Delete of default logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 import Main from './Main';
 import Detail from './Detail';
