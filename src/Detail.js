aee778b019f1e4d402aa9b8492a9406e7d344244
Carlos <aquinoqcd29@gmail.com>
2018-04-17 23:31:20 -0400

    Deatail article component

--- src/Detail.js
+++ src/Detail.js
@@ -1,14 +1,60 @@
 import React, {Component} from 'react';
+import {withStyles} from 'material-ui/styles';
+import Paper from 'material-ui/Paper';
+import Typography from 'material-ui/Typography';
+
+const styles = theme => ({
+  root: theme.mixins.gutters({
+    paddingTop: 16,
+    paddingBottom: 16,
+    marginTop: theme.spacing.unit * 3,
+    width: "100%",
+    textAlign: "center"
+  }),
+  paper: {
+    textAlign: "left",
+    padding: 16,
+  },
+  paperContainer: {
+    maxWidth: 1200,
+    display: "inline-block"
+  },
+  head: {
+    width: "100%",
+    textAlign: "center"
+  }
+});
 
 class Detail extends Component {
   
-    render() {
-      return (
-        <div>
-            Detail article
-        </div>
-      );
-    }
+  componentDidMount() {
+    console.log(this.props);
   }
+
+  render() {
+    const { classes } = this.props;
+    const data = this.props.location.state.content;
+    const featureMedia = this.props.location.state.featureMedia;
+    return (
+      <div className={classes.root}>
+        <div className={classes.paperContainer}>
+        <Paper className={classes.paper} elevation={4}>
+          <div className={classes.head}>
+            <img src={featureMedia.url} style={{height: 300, marginBottom: 16, display: "inline-block"}}/>
+          </div>
+          {
+            data.map((e, index)=> (
+                <div key={index}>
+                  <Typography gutterBottom >{ e.text }</Typography>
+                </div>
+              )
+            )
+          }
+        </Paper>
+        </div>
+      </div>
+    );
+  }
+}
   
-  export default Detail;
+  export default withStyles(styles)(Detail);
